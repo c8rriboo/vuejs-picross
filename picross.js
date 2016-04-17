14763a23c336b31b77603b3521d74dadfb2aca09
Clement BORRALHO <c8rriboo@gmail.com>
2016-04-17 23:44:05 +0200

    'revamp'

---
+++ picross.js
@@ -0,0 +1,104 @@
+new Vue({
+	el: '#picross',
+	
+	data: {
+		single_cell: 0,
+
+		css_classes: {
+			'-1': 'crossed',
+			0: 'empty',
+			1: 'filled',
+		},
+
+		solution: [
+			[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+			[0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			[0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+			[1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+			[1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+			[0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+			[0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+		],
+
+		grid: [
+			[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+			[0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			[0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+			[1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+			[1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+			[0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+			[0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+			[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+		],
+
+		gride_size: {x: 10, y: 10},
+	},
+
+	computed: {
+		valid: function() {
+			var grid = this.grid,
+				solution = this.solution,
+				valid = true;
+
+			for (var line_key = solution.length - 1; line_key >= 0; line_key--) {
+				var line = solution[line_key];
+
+				for (var cell_key = line.length - 1; cell_key >= 0; cell_key--) {
+					var solution_cell = line[cell_key];
+
+					if (solution_cell < 1) {
+						if (grid[line_key][cell_key] > 0) {
+							valid = false;
+							break;
+						};
+					} else {
+						if (solution_cell != grid[line_key][cell_key]) {
+							valid = false;
+							break;
+						};
+					}
+				};
+			};
+
+			return valid;
+		},
+	},
+
+	methods: {
+		updateCell: function(event, line_key, cell_key, right_click) {
+			event.preventDefault();
+
+			var old_value = this.grid[line_key][cell_key],
+				new_value = (right_click) ? -1 : 1;
+
+			if (old_value == new_value) {
+				new_value = 0;
+			}
+
+			this.setCellValue(line_key, cell_key, new_value);
+		},
+
+		setCellValue: function(line_key, cell_key, value) {
+			var line = this.grid[line_key],
+				cell = line[cell_key];
+
+			line.$set(cell_key, value);
+
+			this.grid.$set(line_key, line);
+		},
+
+		getCellCssClasses: function(value) {
+			var cell_class = this.css_classes[value];
+			return ['cell', cell_class];
+		}
+	},
+
+	ready: function () {
+
+	},
+})
